c5f673b81882ccdcd0be4308de3f7f30fcc50f2a
Sanjar Yusupjonov <s.yusupjponov@newuu.uz>
2026-09-18 10:38:41 +0500

    test continuous deployment

--- src/main/java/com/trackly/locationtracking/LocationTrackingApplication.java
+++ src/main/java/com/trackly/locationtracking/LocationTrackingApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties(StopDetectionProperties.class)
 public class LocationTrackingApplication {
-
+    // Employee Location Tracking API
     public static void main(String[] args) {
         SpringApplication.run(LocationTrackingApplication.class, args);
     }
